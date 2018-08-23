63e3416cb59b65483428ed553dd6ba9a4b2b6261
codingkasia <codingkasia@gmail.com>
2018-08-22 20:25:25 -0400

    updated API_ROOT to heroku url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,12 @@
-export const API_ROOT = 'http://localhost:3001';
-export const API_WS_ROOT = 'ws://localhost:3001/cable';
-export const HEADERS = {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
+// local 
+// export const API_ROOT = 'http://localhost:3001';
+export const API_ROOT = ["https://lucky_bidwell_backend.herokuapp.com", "http://lucky_bidwell_backend.herokuapp.com"];
+// # local
+// export const API_WS_ROOT = 'ws://localhost:3001/cable';
+// production
+export const API_WS_ROOT = 'wss://lucky_bidwell_backend.herokuapp.com/cable';
+//
+wss: export const HEADERS = {
+  "Content-Type": "application/json",
+  Accept: "application/json"
 };
