10b43e6f48e9bc12bdb6bd528c63cc5f7d638dda
codingkasia <codingkasia@gmail.com>
2018-08-27 16:55:55 -0400

    update url from local to production in constants/index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,11 @@
 // local 
-export const API_ROOT = 'http://localhost:3001';
+// export const API_ROOT = 'http://localhost:3001';
 // production
-//export const API_ROOT = ["https://lucky_bidwell_backend.herokuapp.com", "http://lucky_bidwell_backend.herokuapp.com"];
+export const API_ROOT = ["https://lucky_bidwell_backend.herokuapp.com", "http://lucky_bidwell_backend.herokuapp.com"];
 // # local
-export const API_WS_ROOT = 'ws://localhost:3001/cable';
+// export const API_WS_ROOT = 'ws://localhost:3001/cable';
 // production
-//export const API_WS_ROOT = 'wss://lucky_bidwell_backend.herokuapp.com/cable';
+export const API_WS_ROOT = 'wss://lucky_bidwell_backend.herokuapp.com/cable';
 //
 export const HEADERS = {
   "Content-Type": "application/json",
